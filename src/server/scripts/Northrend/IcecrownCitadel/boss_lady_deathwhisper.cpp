1cfc6d4f26abb30adf1d641f9dc5a702e02145a6
TBD WoW <wow@tbd.win>
2023-08-12 17:35:11 -0500

    Change lady deathwhisper to whisper less death on 25H

--- src/server/scripts/Northrend/IcecrownCitadel/boss_lady_deathwhisper.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/boss_lady_deathwhisper.cpp
@@ -398,7 +398,7 @@ public:
                         std::random_device rd;
                         std::shuffle(begin, end, std::default_random_engine{rd()});
 
-                        for (uint8 i = 0; i < RAID_MODE<uint8>(0, 1, 1, 3) && i < validPlayers.size(); i++)
+                        for (uint8 i = 0; i < RAID_MODE<uint8>(0, 1, 1, 1) && i < validPlayers.size(); i++)
                         {
                             Unit* target = validPlayers[i];
                             me->CastSpell(target, SPELL_DOMINATE_MIND_25, true);
